fff0883f2d0302ebd20da385061423abe901712e
hikiomoriku <111180165+hikiomoriku@users.noreply.github.com>
2022-08-14 14:19:46 +0300

    Add files via upload
    во 2 изменении заменил strpos() на substr(). По совету программы изменил 2 пункт в 4 строке (вместо Find написал int) в 3 изменении. В 1 части (с find и strpos получил "Данный текст отсутствует в строке". Во 2 части (с int и substr) получил "Текст найден"

--- функция strpos.php
+++ функция strpos.php
@@ -1,6 +1,6 @@
 <?php
 $find="str";
 $txt="sring";
-if (substr($txt,$find)===false) echo "Данный текст остутствует в строке!";
+if (substr($txt,$int)===false) echo "Данный текст остутствует в строке!";
 else echo "Текст найден!"
 ?>
